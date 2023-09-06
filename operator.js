8186aa29f482c6dd1287effd94fa1f27170043d0
Mubashir <mubashiraptech21@gmail.com>
2023-09-07 01:52:16 +0500

    coded

---
+++ operator.js
@@ -0,0 +1,11 @@
+var username ="admin";
+var password ="Admin123";
+var phone =1324234;
+var uname ="admin";
+var pword = "Admin123"
+if((uname == username || uname == phone) && pword == password){
+console.log("log in")
+}
+else{
+    console.log("error in")   
+}
--- script.js
+++ script.js
@@ -1,15 +1,51 @@
+$("#input").on('keydown', function (e) {
+    if (e.keyCode == 13) {
+        $("#search").click();
+    }
+})
+    $("#search").click(function () {
+        $("#row").html('');
+        input = $("#input").val();
+        const url = `http://www.omdbapi.com/?s=${input}&apikey=704c6ce7`;
+        $.ajax({
+            url: url,
+            type: "GET",
+        dataType: "JSON",
+        success: function (accept) {
+            for (i of accept.Search) {
+                $("#row").append(
+                    `<div class="card" style="width: 18rem; margin:auto">
+             <img class="card-img-top" src="${i.Poster}" alt="Card image cap">
+             <div class="card-body">
+                 <h5 class="card-title">${i.Title}</h5>
+                 <p class="card-text">${i.Year}</p>
+                 <p class="card-text">${i.Type}</p>
+                 <a href="#" class="btn btn-primary">${i.imdbID}</a>
+                 </div>
+                 </div>`
+                 )
+                }
+            },
+            error: function (accept) {
+                console.log(accept)
+            }
+        })
+        
+        
+})
+
 const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
 
-toggle.addEventListener('click', function(){
+toggle.addEventListener('click', function () {
     this.classList.toggle('bi-moon');
-    if(this.classList.toggle('bi-brightness-high-fill')){
+    if (this.classList.toggle('bi-brightness-high-fill')) {
         body.style.background = 'white';
         body.style.color = 'black';
-        body.style.transition = '2s';
-    }else{
+        body.style.transition = '0.5s';
+    } else {
         body.style.background = 'black';
         body.style.color = 'white';
-        body.style.transition = '2s';
+        body.style.transition = '0.5s';
     }
 });
