aa8f7f44371b52e8d915c8ccd7964d3ea27e762f
Mubashir <mubashiraptech21@gmail.com>
2023-09-09 01:37:05 +0500

    main

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
- for(i =0 ; i<5; i++){
+//  for(i =0 ; i<5; i++){
+//     for(j=0 ; j<5 ; j++){
+//         document.write("* ")
+//     }
+//     document.write("<br>")
+//  }
+
+for(i=0 ; i<5; i++){
     for(j=0 ; j<5 ; j++){
         document.write("* ")
     }
     document.write("<br>")
- }
+}
--- operator.js
+++ operator.js
@@ -1,12 +1,12 @@
-var username ="admin";
-var password ="Admin123";
+// var username ="21178";
+// var password ="Admin1234";
 
 
-if((username == "admin"|| username == 22117 ) && password == "Admin123" ){
-console.log("log in")
-}
-else{
-    console.log("error in")   
-}
+// if((username == "admin" || username == 2117 ) && password == "Admin123" ){
+// console.log("log in")
+// }
+// else{
+//     console.log("error")   
+// }
 
-console.log((username == "admin"|| username == 22117 ) && password == "Admin123")
+ console.log((username == "admin" || username == '2117' ) && password == "Admin123" ? "logged in!" : "username or password is incorrect")
